2b07b009f55311464d14af7244c11d738501906e
aniketsachan738044-a11y <aniketsachan738044@gmail.com>
2026-09-03 00:16:26 +0530

    Enable permissive CORS headers for Vercel cross-origin requests

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,16 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
